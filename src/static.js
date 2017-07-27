4bbca12c06dd730c1e6daa3fb3552e2ba7b9f9c1
Kevin <regen@anche.no>
2017-07-27 15:50:44 +0200

    Center-aligned button text and replaced decimal with comma for European glory.

--- src/static.js
+++ src/static.js
@@ -3,5 +3,5 @@ export const buttons = [
   [ { text: '7' }, { text: '8' }, { text: '9' }, { text: '-', oper: true } ], 
   [ { text: '4' }, { text: '5' }, { text: '6' }, { text: '+', oper: true } ], 
   [ { text: '1' }, { text: '2' }, { text: '3' }, { text: '=', rowSpan: 2, oper: true } ], 
-  [ { text: '0', colSpan: 2 }, { text: '.' } ]
+  [ { text: '0', colSpan: 2 }, { text: ',' } ]
 ];
